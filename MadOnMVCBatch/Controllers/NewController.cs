7788d05f9136418dec3ec524de11e723820add65
yawar2019 <yawarali17@gmail.com>
2022-03-21 09:07:24 +0530

    commit

--- MadOnMVCBatch/Controllers/NewController.cs
+++ MadOnMVCBatch/Controllers/NewController.cs
@@ -9,7 +9,7 @@ namespace MadOnMVCBatch.Controllers
 {
     public class NewController : Controller
     {
-        // GET: New Work for nidhi is created on 21-03-2022
+        // GET: New Work for vaishnavi is created on 21-03-2022
         public string Index()
         {
             return "india is Nos "+ Index2();
