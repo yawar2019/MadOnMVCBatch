59cd2eb7b98acd5a25b2c37dad45f4b847178e47
yawar2019 <yawarali17@gmail.com>
2022-03-22 09:01:40 +0530

    committed

---
+++ MadOnMVCBatch/Models/EmpDept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MadOnMVCBatch.Models
+{
+    public class EmpDept
+    {
+        public List<EmployeeModel> emp { get; set; }
+        public List<DepartmentModel> dept { get; set; }
+    }
+}
