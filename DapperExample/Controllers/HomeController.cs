ed3f0759fe78adb49a8005cd4ad7ec2ffdd2adbc
yawar2019 <yawarali17@gmail.com>
2022-04-01 09:14:45 +0530

    Done

---
+++ DapperExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DapperExample.Models;
+namespace DapperExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.GetEmployees());
+        }
+    }
+}
---
+++ DapperExample/Models/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+namespace DapperExample.Models
+{
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            var result = con.Query<EmployeeModel>("SELECT * FROM [dbo].[employeeDetails]").ToList();
+            return result;
+
+        }
+
+    }
+}
